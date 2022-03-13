ed3b74df799064107a6bac672e2d6d106b5fc123
JustinW1395 <justinwilcox95@hotmail.com>
2022-03-13 14:15:06 -0700

    fied indentation

--- Django0/faketwitapi/faketwit/urls.py
+++ Django0/faketwitapi/faketwit/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^tweets/$', views.TweetList.as_view(),name=views.TweetList.name),
-     re_path(r'^tweets/(?P<pk>[0-9]+)/$', views.TweetDetail.as_view(), name=views.TweetDetail.name),
+    re_path(r'^tweets/(?P<pk>[0-9]+)/$', views.TweetDetail.as_view(), name=views.TweetDetail.name),
     re_path(r'^account/register/$', views.UserCreate.as_view(),name=views.UserCreate.name),
     re_path(r'^account/register/(?P<pk>[0-9]+)/$', views.UserCreateDetail.as_view(),name=views.UserCreateDetail.name),
     re_path(r'^users/$',views.UserList.as_view(),name=views.UserList.name),
