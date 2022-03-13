53ec2daa3850ed631b3cc4aa54c9b1dfb41bb84b
rjb888 <robert.j.bronson@gmail.com>
2022-03-13 13:54:57 -0700

    adding post tweet endpoint

--- Django0/faketwitapi/faketwit/serializers.py
+++ Django0/faketwitapi/faketwit/serializers.py
@@ -4,16 +4,17 @@ from django.contrib.auth.models import User
 
 
 
-class TweetSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    date = serializers.DateTimeField()
-    tweet_text = serializers.CharField(max_length=250)
+class TweetSerializer(serializers.HyperlinkedModelSerializer):
 
-    def create(self, validated_data):
-        return Tweets.objects.create(**validated_data)
+    author = serializers.ReadOnlyField(source='author.username')
 
-    def update(self, instance, validated_data):
-        instance.tweet_text = validated_data.get('tweet_text', instance.tweet_text)
+    class Meta:
+        model = Tweets
+        fields = (
+                'url',
+                'author',
+                'tweet_text',
+                'date')
 
 class UserCreateSerializer(serializers.ModelSerializer):
 
@@ -29,15 +30,3 @@ class UserCreateSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
-
-'''class UserSerializer(serializers.Serializer):
-    pk = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(max_length=200, required=True)
-    password = serializers.CharField(max_length=200, required=True)
-
-    def create(self, validated_data):
-        return User.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.username = validated_data.get('username', instance.username)
-        instance.password = validated_data.get('password', instance.password)'''
--- Django0/faketwitapi/faketwit/views.py
+++ Django0/faketwitapi/faketwit/views.py
@@ -1,3 +1,4 @@
+import imp
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,6 +12,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.reverse import reverse
 from rest_framework.response import Response
+from rest_framework import permissions
+from .permissions import IsAuthorOrReadOnly, IsFollowOrReadOnly
 
 # Create your views here.
 
@@ -21,39 +24,31 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
-@csrf_exempt
-def tweet_list(request):
-    if request.method == 'GET':
-        tweets = Tweets.objects.all()
-        tweet_serializer = TweetSerializer(tweets, many=True)
-        return JSONResponse(tweet_serializer.data)
-    elif request.method == 'POST':
-        tweet_data = JSONParser().parse(request)
-        tweet_serializer = TweetSerializer(data=tweet_data)
-        if tweet_serializer.is_valid():
-            tweet_serializer.save()
-            return JSONResponse(tweet_serializer.data, status=status.HTTP_201_CREATED)
-        return JSONResponse(tweet_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def tweet_detail(request, pk):
-    try:
-        tweet = Tweets.objects.get(pk=pk)
-    except Tweets.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-    if request.method == 'GET':
-        tweet_serializer = TweetSerializer(tweet)
-        return JSONResponse(tweet_serializer.data)
-    elif request.method == 'PUT':
-        tweet_data = JSONParser().parse(request)
-        tweet_serializer = TweetSerializer(tweet, data=tweet_data)
-        if tweet_serializer.is_valid():
-            tweet_serializer.save()
-            return JSONResponse(tweet_serializer.data)
-        return JSONResponse(tweet_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        tweet.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+class TweetList(generics.ListCreateAPIView):
+    queryset = Tweets.objects.all()
+    serializer_class = TweetSerializer
+    name = 'tweet-list'
+
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        # Pass an additional owner field to the create method
+        # To Set the owner to the user received in the request
+        serializer.save(author=self.request.user)
+
+
+class TweetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tweets.objects.all()
+    serializer_class = TweetSerializer
+    name = 'tweets-detail'
+
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly)
+
 
 class UserCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -72,6 +67,7 @@ class ApiRoot(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         return Response({
             'signup':reverse(UserCreate.name, request=request),
+            'tweets': reverse(TweetList.name, request=request),
             })
 
 
